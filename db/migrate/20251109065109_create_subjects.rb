06deed59eebb28153d8e9485d8ccb239b9bc6160
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-11-09 14:58:27 +0800

    feat: Scaffold Subjects

---
+++ db/migrate/20251109065109_create_subjects.rb
@@ -0,0 +1,10 @@
+class CreateSubjects < ActiveRecord::Migration[8.0]
+  def change
+    create_table :subjects do |t|
+      t.string :name, null: false
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
