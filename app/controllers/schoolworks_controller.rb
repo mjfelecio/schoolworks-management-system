0dd4bdd2c5775868b013827521dd1359960feafd
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-11-17 14:34:18 +0800

    feat: Design schoolworks components
    - Add designs for index and show page for schoolworks
    - Create subject, no_schoolworks, details_placeholder, and schoolwork_item partials
    - Fetch schoolwork, notes, and note in show action in controller
    - Register index page in the sidebar

--- app/controllers/schoolworks_controller.rb
+++ app/controllers/schoolworks_controller.rb
@@ -1,8 +1,12 @@
 class SchoolworksController < ApplicationController
   def index
+    @schoolworks = Schoolwork.all
   end
 
   def show
+    @schoolwork = Schoolwork.find(params[:id])
+    @notes = @schoolwork.notes
+    @note = Note.new
   end
 
   def new
