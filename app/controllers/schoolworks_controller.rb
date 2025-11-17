8274d5588e469d1672279fd27d48b5d4fbef3bc3
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-11-17 21:59:17 +0800

    feat: Implement create and update actions for schoolworks

--- app/controllers/schoolworks_controller.rb
+++ app/controllers/schoolworks_controller.rb
@@ -1,9 +1,9 @@
 class SchoolworksController < ApplicationController
-  before_action :set_subjects, only: [ :edit, :new, :show ]
-  before_action :set_schoolworks, only: [ :index ]
-  before_action :set_schoolwork, only: [ :show, :edit ]
+  before_action :set_subjects, only: [ :edit, :new, :create, :show ]
+  before_action :set_schoolwork, only: [ :show, :edit, :update, :destroy ]
 
   def index
+      @schoolworks = Schoolwork.all
   end
 
   def show
@@ -15,12 +15,32 @@ class SchoolworksController < ApplicationController
   end
 
   def create
+    @schoolwork = Schoolwork.new(schoolwork_params)
+
+    respond_to do |format|
+      if @schoolwork.save
+        format.html { redirect_to :schoolworks, notice: "Schoolwork was successfully created." }
+        format.json { render :index, status: :created, location: @schoolwork }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @schoolwork.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @schoolwork.update(schoolwork_params)
+        format.html { redirect_to @schoolwork, notice: "Schoolwork was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @schoolwork }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @schoolwork.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -28,26 +48,27 @@ class SchoolworksController < ApplicationController
 
   private
 
-  def set_schoolwork
-    @schoolwork = Schoolwork.find(params[:id])
-  end
+    def set_schoolwork
+      @schoolwork = Schoolwork.find(params[:id])
+    end
 
-  def set_schoolworks
-    @schoolworks = Schoolwork.all
-  end
+    def set_subjects
+      @subjects = Subject.all
+    end
 
-  def set_subjects
-    @subjects = Subject.all
-  end
-
-  def schoolwork_params
-    params.expect(schoolwork: [
-      :files,
-      :schoolwork_type,
-      :title,
-      :description
-
-
-    ])
-  end
+    def schoolwork_params
+      params.expect(schoolwork: [
+        :files,
+        :schoolwork_type,
+        :title,
+        :description,
+        :due_date,
+        :status,
+        :priority,
+        :grade,
+        :submitted_at,
+        :subject_id,
+        files: []
+      ])
+    end
 end
